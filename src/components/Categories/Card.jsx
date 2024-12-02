42a6570dd8cbb5f0291bd471e1da5bf4512bbe8d
ifnavneetrajput <navneetrajput404@gmail.com>
2024-12-02 12:53:41 +0530

    adding style to Search and remove index from card

--- src/components/Categories/Card.jsx
+++ src/components/Categories/Card.jsx
@@ -10,7 +10,7 @@ const navigate=useNavigate();
       {
         category.map((item,i) => (
           <div className="single" key={item.id} onClick={()=>{navigate(`/Home/${item.id}`)}}>
-            <h1 className="index">{`${++i}`}</h1>
+      
             <img className="image" src={item.poster} alt=""/>
           </div>
         ))
